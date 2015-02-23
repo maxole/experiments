a6cbd83449e33c7301b0794a98717256003c875f
maxole <_kulta@bk.ru>
2015-02-23 17:32:54 +0400

    add
    add project files

---
+++ DataRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+namespace Core.Repository
+{
+    using Entity;
+    using Serializer;
+
+    public interface IDataRepository
+    {
+        IEnumerable<T> Fetch<T>() where T : IEntity;
+        void Save<T>(T data) where T : IEntity;
+        void Delete<T>(T data) where T : IEntity;
+        void DeleteAll();
+    }
+
+    public class DataRepository : IDataRepository
+    {
+        private readonly Encoding _encoding = Encoding.UTF8;
+
+        public IEnumerable<T> Fetch<T>() where T : IEntity
+        {
+            var path = GetDirectory();
+            return Directory.GetFiles(path, "*.xml").Select(file =>
+            {
+                var content = File.ReadAllText(file, _encoding);
+                return new StringBuilder(content);
+            }).Select(builder => new XmlDataSerializer().Deserialize<T>(builder));
+        }
+
+        public void Save<T>(T data) where T : IEntity
+        {         
+            Delete(data);
+            var xml = new XmlDataSerializer().Serialize(data);            
+            File.WriteAllText(BuildFileName(data), xml.ToString(), _encoding);
+        }
+
+        public void Delete<T>(T data) where T : IEntity
+        {
+            var path = BuildFileName(data);
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+
+        public void DeleteAll()
+        {
+            var path = GetDirectory();
+            Directory.Delete(path, true);
+        }
+
+        private string GetDirectory()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "data");
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            return path;
+        }
+
+        private string BuildFileName(IEntity data)
+        {
+            var path = GetDirectory();
+            return Path.Combine(path, data.SystemId + ".xml");            
+        }
+    }
+}
---
+++ IDataSerializer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+// ReSharper disable once CheckNamespace
+namespace Core.Serializer
+{
+    using Entity;
+
+    /// <summary>
+    /// сериализатор тестовых данных
+    /// </summary>
+    public interface IDataSerializer
+    {
+        /// <summary>
+        /// преобразовать в XML
+        /// </summary>
+        /// <param name="data">данные</param>
+        /// <returns>объект, содержащий сериализованные данные</returns>
+        StringBuilder Serialize<T>(T data) where T :IEntity;
+
+        /// <summary>
+        /// преобразовать из XML в объект
+        /// </summary>
+        /// <typeparam name="T">тип параметр</typeparam>
+        /// <param name="builder">XML данные</param>
+        /// <returns>сериализованный объект</returns>
+        T Deserialize<T>(StringBuilder builder) where T : IEntity;
+    }
+
+    /// <summary>
+    /// сериализатор тестовых данных
+    /// </summary>
+    public class XmlDataSerializer : IDataSerializer
+    {
+        /// <summary>
+        /// преобразовать в XML
+        /// </summary>
+        /// <param name="data">данные</param>
+        /// <returns>объект, содержащий сериализованные данные</returns>
+        public StringBuilder Serialize<T>(T data) where T : IEntity
+        {
+            var builder = new StringBuilder();
+            using (var writer = new StringWriter(builder))
+            {
+                var serializer = new XmlSerializer(data.GetType(), "");
+                serializer.Serialize(writer, data);
+            }            
+            return builder;
+        }
+        /// <summary>
+        /// преобразовать из XML в объект
+        /// </summary>
+        /// <typeparam name="T">тип параметр</typeparam>
+        /// <param name="builder">XML данные</param>
+        /// <returns>сериализованный объект</returns>
+        public T Deserialize<T>(StringBuilder builder) where T : IEntity
+        {
+            using (var reader = new StringReader(builder.ToString()))
+            {
+                var serializer = new XmlSerializer(typeof(T), "");
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+
+        public static void Read(IEntity component, XmlReader reader)
+        {
+            var document = XDocument.Load(reader);
+            if (document.Root == null)
+                throw new Exception("error in reader xml");
+
+            foreach (var element in document.Root.Elements())
+            {
+                var typeName = element.FirstAttribute.Value;
+                var type = Type.GetType(typeName);
+                if (type == null)
+                    throw new Exception("type is null");
+                if (!type.Assembly.GlobalAssemblyCache)
+                {
+                    var serializer = new XmlSerializer(type);
+                    var e = new XElement(element.Name.LocalName, element.Elements());
+                    var o = serializer.Deserialize(e.CreateReader());
+                    var list = (component as IList);
+                    if (list != null)
+                        list.Add(o);
+                }
+                else
+                {
+                    var property = component.GetType().GetProperty(element.Name.LocalName);
+                    var value =
+                        TypeDescriptor.GetConverter(property.PropertyType).ConvertFromInvariantString(element.Value);
+                    if (property.CanWrite)
+                        property.SetValue(component, value, null);
+                    /*
+                    component.GetType()
+                        .InvokeMember(property.Name,
+                            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty |
+                            BindingFlags.Instance,
+                            null, component, new object[] {value});
+                    */
+                }
+            }
+        }
+
+        public static void Write(IEntity component, XmlWriter writer)
+        {
+            var type = component.GetType();
+            foreach (var property in type
+                .GetProperties()
+                .Where(p => p.GetCustomAttributes(typeof(XmlElementAttribute), true).Any()))
+            {
+                var tmp = property.GetValue(component, null);
+                if (tmp == null)
+                    continue;
+                var value = tmp.ToString();
+                writer.WriteElementString("i", property.Name, property.PropertyType.FullName, value);
+            }
+
+            var enumerable = (component as IEnumerable);
+            if (enumerable == null)
+                return;
+            foreach (var r in enumerable)
+            {
+                var serializer = new XmlSerializer(r.GetType(), r.GetType().AssemblyQualifiedName);
+                serializer.Serialize(writer, r);
+            }
+        }
+    }
+}
