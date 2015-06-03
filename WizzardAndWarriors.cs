537131bb22ef6d435b8021a0ec032124c574f1fa
maxole <_kulta@bk.ru>
2015-06-03 10:02:07 +0300

    fix
    fix namespace

--- WizzardAndWarriors.cs
+++ WizzardAndWarriors.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject5_1
+namespace WizzardAndWarriors.Test
 {
     [TestClass]
     public class WizzardAndWarriors
