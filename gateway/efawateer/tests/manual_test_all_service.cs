f82832e6f5c18931392c82dcbb9a1b51c2f73baa
maxole <_kulta@bk.ru>
2016-02-09 09:18:33 +0300

    refact
    refactoring tests

--- gateway/efawateer/tests/manual_test_all_service.cs
+++ gateway/efawateer/tests/manual_test_all_service.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.ServiceModel;
 using System.Xml.Linq;
 using EfawateerTests.Proxy;
@@ -12,19 +13,24 @@ namespace EfawateerTests
     [TestClass]
     public class manual_test_all_service
     {
-        [TestMethod]
-        public void BillerList()
+        private string GetToken()
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
 
             var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
             var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
+            return element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
+        }
 
+        [TestMethod]
+        public void BillerList()
+        {
+
+            var binding = new WSHttpBinding(SecurityMode.None, true);
             var client = new BillersListClient(binding, new EndpointAddress(UriContext.BillersList));
-            var list = client.GetBillersList(Guid.NewGuid().ToString(), key);   
+            var list = client.GetBillersList(Guid.NewGuid().ToString(), GetToken());
 
-            Debug.WriteLine("BillerList " +list);
+            Debug.WriteLine("BillerList " + list);
         }
 
         [TestMethod]
@@ -32,18 +38,15 @@ namespace EfawateerTests
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
 
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
-
             var request = XElement.Parse(GetContentFromFile("AccountUpload.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
 
             request.Element("MsgHeader").Element("TmStp").Value = DateTime.Now.ToString("s");
-            request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
+            request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new AccountUploadClient(binding, new EndpointAddress(UriContext.AccountUpload));
-            var upload = client.AccountUpload(Guid.NewGuid().ToString(), key, request);
+            var upload = client.AccountUpload(Guid.NewGuid().ToString(), GetToken(), request);
 
             Debug.WriteLine("AccountUpload" + upload);
         }
@@ -53,18 +56,15 @@ namespace EfawateerTests
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
 
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
-
             var request = XElement.Parse(GetContentFromFile("AccountInquiry.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
 
             request.Element("MsgHeader").Element("TmStp").Value = DateTime.Now.ToString("s");
-            request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
+            request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new AccountInquiryClient(binding, new EndpointAddress(UriContext.AccountInquiry));
-            var upload = client.AccountInquiry(Guid.NewGuid().ToString(), key, request);
+            var upload = client.AccountInquiry(Guid.NewGuid().ToString(), GetToken(), request);
 
             Debug.WriteLine("AccountInquiry " + upload);
         }
@@ -74,18 +74,15 @@ namespace EfawateerTests
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
 
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
-
             var request = XElement.Parse(GetContentFromFile("AddCustomerBilling.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
 
             request.Element("MsgHeader").Element("TmStp").Value = DateTime.Now.ToString("s");
-            request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
+            request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new AddCustomerBillingClient(binding, new EndpointAddress(UriContext.AddCustomerBilling));
-            var upload = client.AddCustomerBilling(Guid.NewGuid().ToString(), key, request);
+            var upload = client.AddCustomerBilling(Guid.NewGuid().ToString(), GetToken(), request);
 
             Debug.WriteLine("AddCustomerBilling " + upload);
         }
@@ -95,21 +92,19 @@ namespace EfawateerTests
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
 
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
-
             var request = XElement.Parse(GetContentFromFile("BillPayment.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
 
             var time = DateTime.Now.ToString("s");
             request.Element("MsgHeader").Element("TmStp").Value = time;
             request.Element("MsgBody").Element("Transactions").Element("TrxInf").Element("ProcessDate").Value = time;
-            request.Element("MsgBody").Element("Transactions").Element("TrxInf").Element("BankTrxID").Value = Guid.NewGuid().ToString();
-            request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
+            request.Element("MsgBody").Element("Transactions").Element("TrxInf").Element("BankTrxID").Value =
+                Guid.NewGuid().ToString();
+            request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new PaymentClient(binding, new EndpointAddress(UriContext.BillPayment));
-            var upload = client.PayBill(Guid.NewGuid().ToString(), key, request);
+            var upload = client.PayBill(Guid.NewGuid().ToString(), GetToken(), request);
 
             Debug.WriteLine("BillPayment " + upload);
         }
@@ -119,19 +114,16 @@ namespace EfawateerTests
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
 
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
-
             var request = XElement.Parse(GetContentFromFile("BillInquiry.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
 
             var time = DateTime.Now.ToString("s");
             request.Element("MsgHeader").Element("TmStp").Value = time;
-            request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
+            request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new BillInquiryClient(binding, new EndpointAddress(UriContext.BillInquiry));
-            var upload = client.Inquire(Guid.NewGuid().ToString(), key, request);
+            var upload = client.Inquire(Guid.NewGuid().ToString(), GetToken(), request);
 
             Debug.WriteLine("BillInquiry " + upload);
         }
@@ -140,10 +132,6 @@ namespace EfawateerTests
         public void PaymentInquiry()
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
-
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
 
             var request = XElement.Parse(GetContentFromFile("PaymentInquiry.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
@@ -156,7 +144,7 @@ namespace EfawateerTests
             request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new PaymentInquiryClient(binding, new EndpointAddress(UriContext.PaymentInquiry));
-            var upload = client.Inquire(Guid.NewGuid().ToString(), key, request);
+            var upload = client.Inquire(Guid.NewGuid().ToString(), GetToken(), request);
 
             Debug.WriteLine("PaymentInquiry " + upload);
         }
@@ -165,10 +153,6 @@ namespace EfawateerTests
         public void PrepaidPayment()
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
-
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
 
             var request = XElement.Parse(GetContentFromFile("PrepaidPayment.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
@@ -179,10 +163,11 @@ namespace EfawateerTests
             request.Element("MsgHeader").Element("GUID").Value = guid;
             request.Element("MsgBody").Element("TrxInf").Element("ProcessDate").Value = time;
             request.Element("MsgBody").Element("TrxInf").Element("BankTrxID").Value = guid;
-            request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
+            request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new PrepaidPaymentClient(binding, new EndpointAddress(UriContext.PrepaidPayment));
-            var upload = client.Pay(guid, key, request);
+            var upload = client.Pay(guid, GetToken(), request);
 
             Debug.WriteLine("PrepaidPayment " + upload);
         }
@@ -192,10 +177,6 @@ namespace EfawateerTests
         {
             var binding = new WSHttpBinding(SecurityMode.None, true);
 
-            var token = new TokenServiceClient(binding, new EndpointAddress(UriContext.Authenticate));
-            var element = token.Authenticate(Guid.NewGuid().ToString(), 158, "Test@1234");
-            var key = element.Element("MsgBody").Element("TokenConf").Element("TokenKey").Value;
-
             var request = XElement.Parse(GetContentFromFile("PrepaidValidation.xml"));
             var sign = new EfawateerSigner("ARKAIM-TEST");
 
@@ -203,17 +184,65 @@ namespace EfawateerTests
             var guid = Guid.NewGuid().ToString();
             request.Element("MsgHeader").Element("TmStp").Value = time;
             request.Element("MsgHeader").Element("GUID").Value = guid;
-            request.Element("MsgFooter").Element("Security").Element("Signature").Value = sign.SignData(request.Element("MsgBody").ToString());
+            request.Element("MsgBody").Element("BillingInfo").Element("AcctInfo").Element("BillerCode").Value = "21";
+            request.Element("MsgBody").Element("BillingInfo").Element("ServiceTypeDetails").Element("ServiceType").Value = "Prepaid";
+            request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                sign.SignData(request.Element("MsgBody").ToString());
 
             var client = new PrepaidValidationClient(binding, new EndpointAddress(UriContext.PrepaidValidation));
-            var upload = client.Validate(guid, key, request);
+            var upload = client.Validate(guid, GetToken(), request);
 
             Debug.WriteLine("PrepaidValidation " + upload);
         }
 
         private string GetContentFromFile(string name)
         {
-            return File.ReadAllText("requests\\"+name);
+            return File.ReadAllText("requests\\" + name);
+        }
+
+        [TestMethod]
+        public void PrepaidValidationToAll()
+        {
+            var binding = new WSHttpBinding(SecurityMode.None, true);
+            var client = new BillersListClient(binding, new EndpointAddress(UriContext.BillersList));
+            var list = client.GetBillersList(Guid.NewGuid().ToString(), GetToken());
+
+            var billers = list.Element("MsgBody").Element("BilrsRec").Elements().Select(element => new
+            {
+                code = element.Element("BillerCode").Value,
+                services = element.Element("Services").Elements().Where(w => w.Element("PrepaidCats") == null).Select(s => s.Element("ServiceType").Value)
+            }).OrderBy(o=>o.code);//.ToLookup(k=>k.code, k=>k.services);
+
+            var request = XElement.Parse(GetContentFromFile("PrepaidValidation.xml"));
+            var sign = new EfawateerSigner("ARKAIM-TEST");
+
+            foreach (var biller in billers)
+            {
+                var time = DateTime.Now.ToString("s");
+                var guid = Guid.NewGuid().ToString();
+
+                request.Element("MsgHeader").Element("TmStp").Value = time;
+                request.Element("MsgHeader").Element("GUID").Value = guid;
+                request.Element("MsgBody").Element("BillingInfo").Element("AcctInfo").Element("BillerCode").Value = biller.code;
+
+                foreach (var serviceType in biller.services)
+                {
+                    request.Element("MsgBody")
+                        .Element("BillingInfo")
+                        .Element("ServiceTypeDetails")
+                        .Element("ServiceType")
+                        .Value = serviceType;//"Prepaid";
+                    request.Element("MsgFooter").Element("Security").Element("Signature").Value =
+                        sign.SignData(request.Element("MsgBody").ToString());
+
+                    var validator = new PrepaidValidationClient(binding, new EndpointAddress(UriContext.PrepaidValidation));
+                    var upload = validator.Validate(guid, GetToken(), request);
+
+                    Debug.WriteLine(string.Format("-----------{0}--------------", biller.code));
+                    Debug.WriteLine("request:\r\n " +request.Element("MsgBody"));
+                    Debug.WriteLine("response:\r\n " + upload.Element("MsgBody"));                    
+                }                
+            }
         }
     }
 }
